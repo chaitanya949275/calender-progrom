2d3f903e5e6fc3ff94f3659e5b9c31c23394e382
chaitanya949275 <115361706+chaitanya949275@users.noreply.github.com>
2022-11-24 10:59:47 +0530

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,5 @@
+# calendar program
+import calendar     # importing calendar module
+a = int(input('enter the year:')) # to take input from the user
+b = int(input('enter month:'))
+print(calendar.year(a,b)) #display the calendar
